bc8b584c93a5b7b4281edb89ed5df1e1f534c15f
Syed Muhammad Shayan <122820978+Syed-Shayan-01@users.noreply.github.com>
2023-12-19 23:05:19 +0500

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://MyPrac:1234@cluster0.olutjd9.mongodb.net/Authentication?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://"":""@cluster0.olutjd9.mongodb.net/Authentication?retryWrites=true&w=majority');
 module.exports = mongoose;
